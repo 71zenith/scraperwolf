c173caa427b09194a97753e9a09590cadd4cc768
zen <71zenith@proton.me>
2024-07-02 14:37:06 +0530

    initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      librewolf = pkgs.librewolf.override {
+        extraPrefs = ''
+          pref("devtools.source-map.client-service.enabled", false);
+          pref("librewolf.console.logging_disabled", true)
+          pref("devtools.toolbox.host", "window");
+          pref("privacy.resistFingerprinting", false);
+          pref("webgl.disabled", false);
+          pref("librewolf.debugger.force_detach", true);
+        '';
+      };
+    in {
+      packages = {
+        inherit librewolf;
+        default = librewolf;
+      };
+    });
+}
